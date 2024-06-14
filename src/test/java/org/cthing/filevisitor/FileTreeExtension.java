f32355a08688abccd6576c1a0df6819902be2fd4
Baron Roberts <baron@cthing.com>
2024-06-13 17:11:33 -0700

    Use the SPDX license header. Fix a few warnings.

--- src/test/java/org/cthing/filevisitor/FileTreeExtension.java
+++ src/test/java/org/cthing/filevisitor/FileTreeExtension.java
@@ -1,17 +1,6 @@
 /*
  * Copyright 2024 C Thing Software
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.cthing.filevisitor;
@@ -21,6 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
@@ -135,7 +125,9 @@ class FileTreeExtension implements BeforeEachCallback, AfterEachCallback, Parame
 
     private static void deleteAll(final Path root) throws IOException {
         if (Files.exists(root)) {
-            Files.walk(root).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            try (Stream<Path> fileStream = Files.walk(root)) {
+                fileStream.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            }
         }
     }
 }
--- src/test/java/org/cthing/filevisitor/TestHomeExtension.java
+++ src/test/java/org/cthing/filevisitor/TestHomeExtension.java
@@ -1,17 +1,6 @@
 /*
  * Copyright 2024 C Thing Software
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.cthing.filevisitor;
@@ -20,6 +9,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -38,7 +28,9 @@ class TestHomeExtension implements BeforeEachCallback {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public void beforeEach(final ExtensionContext context) throws Exception {
         if (Files.exists(TEST_HOME)) {
-            Files.walk(TEST_HOME).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            try (Stream<Path> fileStream = Files.walk(TEST_HOME)) {
+                fileStream.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            }
         }
         Files.createDirectories(TEST_HOME);
     }
